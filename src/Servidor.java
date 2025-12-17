6539eec96e3ea30633bb6f130348f68f6f00c79f
santiagoolsa <137781282+santiagoolsa@users.noreply.github.com>
2025-12-17 18:08:14 +0100

    actualizacion
    Signed-off-by: santiagoolsa <137781282+santiagoolsa@users.noreply.github.com>

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,48 +1,62 @@
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 
 
 public class Servidor {
     public static void main(String[] args) throws IOException {
 
-        //defino el servidor
+        //defino el servidor TCP y el puerto 5000
         ServerSocket server = new ServerSocket(5000);
         System.out.println("Servidor TCP listo");
 
+        //mientras  haya un servidor que acepte entrara y creará un cliente y lo inicia
+        //por cada cliente crea un hilo
         while (true) {
+
+            //se acepta la conexion
             Socket cliente = server.accept();
+
+            //se crea un hilo
             new Thread(() -> handleClient(cliente)).start();
         }
     }
 
     private static void handleClient(Socket cliente) {
         try {
-            BufferedReader in = new BufferedReader(
+
+            //leo los datos recibidos por el cliente
+            BufferedReader entrada_datos_cliente = new BufferedReader(
                     new InputStreamReader(cliente.getInputStream()));
-            PrintWriter out = new PrintWriter(cliente.getOutputStream(), true);
+
+            //envvio datos al cliente
+            PrintWriter salida_datos_cliente = new PrintWriter(cliente.getOutputStream(), true);
 
             Random r = new Random();
-            int secret = r.nextInt(100) + 1;
+            int numero_introducido = r.nextInt(100) + 1;
 
-            out.println("Adivina el numero (1-100)");
+            salida_datos_cliente.println("Adivina el número (1-100)");
 
-            String line;
-            while ((line = in.readLine()) != null) {
-                int guess;
+            String linea;
+            while ((linea = entrada_datos_cliente.readLine()) != null) {
+                int numero_secreto;
 
                 try {
-                    guess = Integer.parseInt(line);
+                    numero_secreto = Integer.parseInt(linea);
                 } catch (NumberFormatException e) {
-                    out.println("Numero invalido");
+                    salida_datos_cliente.println("Número incorrecto");
                     continue;
                 }
 
-                if (guess < secret) out.println("El numero es mayor");
-                else if (guess > secret) out.println("El numero es menor");
+                if (numero_secreto < numero_introducido) salida_datos_cliente.println("El número es mayor");
+                else if (numero_secreto > numero_introducido) salida_datos_cliente.println("El número es menor");
                 else {
-                    out.println("Numero correcto. Nuevo juego");
-                    secret = r.nextInt(100) + 1;
+                    salida_datos_cliente.println("número correcto.\rNuevo juego\rIntroduce númoero: ");
+                    numero_introducido = r.nextInt(100) + 1;
                 }
             }
 
@@ -53,3 +67,4 @@ public class Servidor {
     }
 
 }
+//subida con conmits
